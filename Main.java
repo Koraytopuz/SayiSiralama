345cd765e12a5ee9df0cb03febb20e62fc6daeb9
Koray <91742967+Koraytopuz@users.noreply.github.com>
2023-03-30 13:09:19 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner inp=new Scanner(System.in);
+        System.out.println("Birinci Sayıyı Giriniz: ");
+        a= inp.nextInt();
+        System.out.println("İkinci Sayıyı Giriniz: ");
+        b= inp.nextInt();
+        System.out.println("Üçüncü Sayıyı Giriniz: ");
+        c= inp.nextInt();
+        if ((a<b)&&(a<c))
+        {
+            if (b<c)
+            {
+                System.out.println("a<b<c");
+            }
+            else
+            {
+                System.out.println("a<c<b");
+            }
+        }
+        else if ((b<a)&&(b<c))
+        {
+            if (a<c)
+            {
+                System.out.println("b<a<c");
+            }
+            else
+            {
+                System.out.println("b<c<a");
+            }
+        }
+        else if ((c<a)&&(c<b))
+        {
+            if (a<b)
+            {
+                System.out.println("c<a<b");
+            }else
+            {
+                System.out.println("c<b<a");
+            }
+        }
+    }
+}
